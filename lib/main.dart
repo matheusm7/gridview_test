121c4064b9e698d20adbecb6de243245e6e8bca6
matheus <matheusidmelo@gmail.com>
2023-03-10 16:39:13 -0300

    arquivo

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MasonryGridView.builder(
+          itemCount: 10,
+          gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (
+            context,
+            index,
+          ) =>
+              Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset('lib/images/image${index + 1}.jpg'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
